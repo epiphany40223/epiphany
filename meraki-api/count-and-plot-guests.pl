1f1a72b8021151da8daee6a6684f67608ff04577
Jeff Squyres <jeff@squyres.com>
2017-04-23 22:28:19 -0400

    count-and-plot-guests: handle newer data
    We've added more Meraki APs.  And expand the script to be a bit more
    friendly to large amounts of data.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- meraki-api/count-and-plot-guests.pl
+++ meraki-api/count-and-plot-guests.pl
@@ -2,8 +2,10 @@
 
 use strict;
 
+use Data::Dumper;
 use POSIX;
 use DateTime;
+use DBI;
 
 my $sqlite_file;
 # On WOPR / queeg
@@ -62,21 +64,24 @@ sub calc_gmt_offset {
 
 #######################################################################
 
+my $dbh = DBI->connect("dbi:SQLite:dbname=$sqlite_file", undef, undef, {
+#    sqlite_open_flags => SQLITE_OPEN_READONLY,
+		       });
+die "Can't open database"
+    if (!$dbh);
+
+my $sql_base  = "select distinct clientMac,ipv4,manufacturer from data where";
+my $sql_next .= "ssid='Epiphany (pw=epiphany)' and ipv4 != '/0.0.0.0' and seenEpoch >= ? and seenEpoch < ? ";
+my $sql_mac   = "$sql_base apMac = ? and $sql_next order by apMac";
+my $sql_nomac = "$sql_base $sql_next";
+
+my $sth_mac   = $dbh->prepare($sql_mac);
+my $sth_nomac = $dbh->prepare($sql_nomac);
+
 sub doit {
     my $apMac = shift;
     my $apName = shift;
     my $dt = shift;
-
-    my $date_str = $dt->strftime("%Y-%m-%d");
-    print "=== For $apName on $date_str\n";
-
-    my $file = "bogus.sql";
-    open(TMP, ">$file")
-	|| die "Can't write to $file";
-
-    my $sql = "select distinct clientMac,ipv4,manufacturer from data where ";
-    $sql .=  "apMac = '$apMac' and "
-	if ($apMac ne "");
 
     # Create a timestamp range that we want for this specific date,
     # and ensure to account for the GMT offset.
@@ -84,27 +89,21 @@ sub doit {
     my $ts_start = $dt->epoch() - $offset;
     my $ts_end = $ts_start + (24 * 60 * 60);
 
-    $sql .= "ssid='Epiphany (pw=epiphany)' and ipv4 != '/0.0.0.0' and seenEpoch >= $ts_start and seenEpoch < $ts_end ";
-    $sql .= "order by apMac"
-	if ($apMac ne "");
-    $sql .= ";";
-
-    print "SQL: $sql\n";
-    print TMP "$sql\n";
-    close(TMP);
-
-    open(SQL, "sqlite3 $sqlite_file -init $file|")
-	|| die "Can't run SQL";
-
     my $count = 0;
-    while (<SQL>) {
-	print $_;
-	++$count;
+    if ($apMac ne "") {
+	$sth_mac->execute($apMac, $ts_start, $ts_end);
+	while ($sth_mac->fetchrow_array()) {
+	    ++$count;
+	}
+    } else {
+	$sth_nomac->execute($ts_start, $ts_end);
+	while ($sth_nomac->fetchrow_array()) {
+	    ++$count;
+	}
     }
-    close(SQL);
-    print "=== Total of $count rows\n";
 
-    unlink($file);
+    my $date_str = $dt->strftime("%Y-%m-%d-%a");
+    print "=== $date_str $apName: $count guests\n";
 
     return $count;
 }
@@ -129,40 +128,83 @@ my $dt = DateTime->new(
     time_zone  => $local_time_zone,
     );
 
+my $meraki_move = DateTime->new(
+    year       => 2016,
+    month      => 10,
+    day        => 16,
+    hour       => 0,
+    minute     => 0,
+    second     => 0,
+    nanosecond => 0,
+    time_zone  => $local_time_zone,
+    );
+
 # Collect results for each date and each location
 my $results;
+my $count = 0;
 while ($dt->epoch() < time()) {
     my $date_str = $dt->strftime("%Y-%m-%d-%a");
 
-    $results->{$date_str}->{wc} =
-	doit("00:18:0a:79:a5:e2", "WC", $dt);
-    $results->{$date_str}->{eh} =
-	doit("00:18:0a:79:8e:2d", "EH Copyroom", $dt);
-    $results->{$date_str}->{both} =
-	doit("", "Both", $dt);
+    # From when we started collecting Meraki data, we had 2 Meraki
+    # WAPs at these MAC addresses:
+    if (DateTime->compare($dt, $meraki_move) <= 0) {
+	$results->{$date_str}->{wc} =
+	    doit("00:18:0a:79:a5:e2", "WC", $dt);
+	$results->{$date_str}->{ehcr} =
+	    doit("00:18:0a:79:8e:2d", "EH Copyroom", $dt);
+	$results->{$date_str}->{all} =
+	    doit("", "All", $dt);
+    }
+    # As of 2016-10-16, we installed several more Meraki APs and moved
+    # the the WC AP to EH phone room.  For this script, just gather
+    # stats for these APs:
+    else {
+	$results->{$date_str}->{wc} =
+	    doit("e0:55:3d:91:a8:b0", "WC", $dt);
+	$results->{$date_str}->{cc} =
+	    doit("e0:55:3d:92:84:a0", "CC", $dt);
+	$results->{$date_str}->{ehcr} =
+	    doit("00:18:0a:79:8e:2d", "EH Copyroom", $dt);
+	$results->{$date_str}->{all} =
+	    doit("", "All", $dt);
+    }
 
     my $e = $dt->epoch() + (24 * 60 * 60);
     $dt = DateTime->from_epoch(epoch => $e);
+    ++$count;
 }
 
 #######################################################################
 
+# Save the raw data
+my $file;
+$file = "results.dump";
+unlink($file);
+open(OUT, ">$file")
+    || die "Can't write to $file";
+print OUT Dumper($results);
+close(OUT);
+
 # Remove the old data file, write a new one
-my $file = "results.txt";
+$file = "results.txt";
 unlink($file);
 open(OUT, ">$file")
     || die "Can't write to $file";
 
 # Grab all the dates and locations
 my @dates = sort(keys(%{$results}));
-my @locations = sort(keys(%{$results->{$dates[0]}}));
+my @locations = sort(keys(%{$results->{$dates[$count - 1]}}));
 
 # Write all the data
 my $num = 1;
 foreach my $date (@dates) {
     print OUT "$date $num ";
     foreach my $location (@locations) {
-	print OUT "$results->{$date}->{$location} ";
+	if (exists($results->{$date}->{$location})) {
+	    print OUT "$results->{$date}->{$location} ";
+	} else {
+	    print OUT "0 ";
+	}
     }
     print OUT "\n";
     ++$num;
@@ -180,14 +222,22 @@ set title "Guests on ECC Meraki AP wifi networks"
 set grid
 set xlabel "Date"
 set ylabel "Number of guests"
-set key bottom left
+set key top left
 
-set xtics border in scale 1,0.5 nomirror rotate by -45  autojustify
+set xtics border in scale 1,0.5 nomirror rotate by -45 autojustify
 set xtics (';
 # Write out the dates for each X tic label
 my $num = 1;
+my $count = 0;
 foreach my $date (@dates) {
-    $gp .= "\"$date\" $num";
+    # Is this a major (0) or minor (1) tic?
+    my $level = 1;
+    if ($count % 7 == 0) {
+	$level = 0;
+    }
+    ++$count;
+
+    $gp .= "\"$date\" $num $level";
     $gp .= ", "
 	if ($num <= $#dates);
     ++$num;
@@ -200,7 +250,7 @@ plot ';
 # Plot each AP
 my $column = 3;
 foreach my $location (@locations) {
-    $gp .= "\"$file\" using 2:$column with linespoints title \"$location\",";
+    $gp .= "\"$file\" using 2:$column with lines title \"$location\",";
     ++$column;
 }
 
