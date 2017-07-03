0d244a60daeb825da4ae019121a6fe84183fa277
Jeff Squyres <jeff@squyres.com>
2017-07-03 14:48:28 -0400

    find-and-copy: updates
    Add a lot of comments to the top of the script explaining what it
    does.
    
    Also, allow exceptions to go back to main(), which will attempt to
    re-authenticate to Google.  If that fails 3 times within 60 seconds
    (admittedly a fairly arbitrary number), let a human figure it out.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- copy-mp3s-to-google-drive/find-and-copy.py
+++ copy-mp3s-to-google-drive/find-and-copy.py
@@ -1,25 +1,73 @@
 #!/usr/bin/env python
 
-# First, need to install the Google API python client:
-#     pip install --upgrade google-api-python-client
+"""Script to upload specific MP3 files to Google Drive.
 
-# This is very helpful to read:
-# https://developers.google.com/identity/protocols/OAuth2
-# We are using the "Installed Applications" scheme.
+- Watch a directory for new MP3 files named of the form
+  Txxx-YYYYMMDD-HHMMSS.mp3.
+- If the file size of the file doesn't change for 30 seconds, assume
+  that the file has finished uploading to this directory, and upload
+  it to a Google Team Drive folder.
+- Put the uploaded MP3 file in a YYYY/MM-MONTHNAME folder, just to
+  break it up.  Create those folders in the Google Team Drive if they
+  don't exist.
+- If the upload fails, it should just try again later.
+- If the upload succeeds, the MP3 file will be moved to the "Uploaded"
+  sub-folder.  Someday, we can likely remove the file altogether, but
+  until we run this script in real-world conditions for a while, we
+  don't know what the failure conditions will be.  So save the MP3s on
+  the local disk for now.
 
-# This is very helpful to read:
-# https://developers.google.com/api-client-library/python/start/get_started
-# We are using Authorized API access (OAuth 2.0).
+Various names and constants are hard-coded into this script, which may
+well be sufficient.
 
-# Steps:
-# 1. Request a(n application) token
-# 2. Provider user consent
-# 3. Get an authorization code from Google
-# 4. Send the code to Google
-# 5. Receive access token and refresh token
-# 6. Can make API calls with the access token
-# 7. If the access token is expired, use the refresh token.  If the
-#    refresh token doesn't work, go back to step 2 (or step 1?).
+Things that still need to be done:
+
+- This script errors out in a few places.  Since this script is
+  intended to be run non-interactively, it should probably send an
+  alert of some kind (email?) to humans when that happens.  I.e., fill
+  in the notification part in diediedie().
+
+-----
+
+This script requires a "client_id.json" file with the app credentials
+from the Google App dashboard.  This file is not committed here in git
+for obvious reasons (!).
+
+This script will create a "user-credentials.json" file in the same
+directory with the result of getting user consent for the Google
+Account being used to authenticate.
+
+Note that this script works on Windows, Linux, and OS X.  But first,
+you need to install the Google API python client:
+
+    pip install --upgrade google-api-python-client
+
+Regarding Google Drive / Google Python API documentation:
+
+1. In terms of authentication, this is very helpful to read:
+
+    https://developers.google.com/identity/protocols/OAuth2
+
+This script is using the "Installed Applications" scheme.
+
+2. In terms of the Google Python docs, this is very helpful to read:
+
+    https://developers.google.com/api-client-library/python/start/get_started
+
+We are using Authorized API access (OAuth 2.0).
+
+Steps:
+
+2a. Request a(n application) token
+2b. Provider user consent
+2c. Get an authorization code from Google
+2d. Send the code to Google
+2e. Receive access token and refresh token
+2f. Can make API calls with the access token
+2g. If the access token is expired, use the refresh token.  If the
+    refresh token doesn't work, go back to step 2 (or step 1?).
+
+"""
 
 import json
 import sys
@@ -46,6 +94,9 @@ target_team_drive = 'ECC Recordings'
 folder_mime_type = 'application/vnd.google-apps.folder'
 source_directory = 'C:\ftp\ecc-recordings'
 mp3_mime_type = 'audio/mpeg'
+dir_check_frequency = 30
+auth_grace_period = 60
+auth_max_attempts = 3
 # Scopes documented here:
 # https://developers.google.com/drive/v3/web/about-auth
 scope = 'https://www.googleapis.com/auth/drive'
@@ -55,16 +106,24 @@ source_directory = '.'
 
 #-------------------------------------------------------------------
 
+def diediedie(msg):
+    print(msg)
+    print("Aborting")
+
+    # JMS Somehow let a human know
+
+    exit(1)
+
+#-------------------------------------------------------------------
+
 def load_app_credentials():
     # Read in the JSON file to get the client ID and client secret
     cwd  = os.getcwd()
     file = os.path.join(cwd, app_cred_file)
     if not os.path.isfile(file):
-	print("Error: JSON file {0} does not exist".format(file))
-	exit(1)
+	diediedie("Error: JSON file {0} does not exist".format(file))
     if not os.access(file, os.R_OK):
-	print("Error: JSON file {0} is not readable".format(file))
-	exit(1)
+	diediedie("Error: JSON file {0} is not readable".format(file))
 
     with open(file) as data_file:
 	app_cred = json.load(data_file)
@@ -156,52 +215,42 @@ def create_folder(service, team_drive, folder_name, parent_id):
 def find_or_create_folder(service, team_drive, folder_name, parent_id):
     # Find a folder identified by this name/parent.  If it doesn't
     # exist, create it.
-    try:
-	# Don't worry about pagination, because we expect to only get
-	# 0 or 1 results back.
-	query = ("name='{0}' and mimeType='{1}' and trashed=false"
-		 .format(folder_name, folder_mime_type))
-	if parent_id is not None:
-	    query = query + (" and '{0}' in parents"
-			     .format(parent_id))
-	print("=== Folder query: {0}".format(query))
-	response = (service.files()
-		    .list(q=query,
-			  spaces='drive',
-			  corpora='teamDrive',
-			  fields='files(name,id,parents)',
-			  teamDriveId=team_drive['id'],
-			  includeTeamDriveItems=True,
-			  supportsTeamDrives=True).execute())
-	folders = response.get('files', [])
+    # Don't worry about pagination, because we expect to only get
+    # 0 or 1 results back.
+    query = ("name='{0}' and mimeType='{1}' and trashed=false"
+	     .format(folder_name, folder_mime_type))
+    if parent_id is not None:
+	query = query + (" and '{0}' in parents"
+			 .format(parent_id))
+    print("=== Folder query: {0}".format(query))
+    response = (service.files()
+		.list(q=query,
+		      spaces='drive',
+		      corpora='teamDrive',
+		      fields='files(name,id,parents)',
+		      teamDriveId=team_drive['id'],
+		      includeTeamDriveItems=True,
+		      supportsTeamDrives=True).execute())
+    folders = response.get('files', [])
 
-	# If we got more than 1 result back, let a human figure it out
-	if len(folders) > 1:
-	    print('Error: found more than one folder matching name="{0}", parents={1}!'.format(folder_name, parent_id))
-	    print('Error: a human should fix this in the Google Drive web interface.')
-	    print('Aborting')
-	    exit(1)
+    # If we got more than 1 result back, let a human figure it out
+    if len(folders) > 1:
+	diediedie("""Error: found more than one folder matching name="{0}"
+Error: a human should fix this in the Google Drive web interface."""
+		  .format(folder_name, parent_id))
 
-	# If we got 0 results back, then go create that folder
-	elif len(folders) == 0:
-	    print("=== Folder not found")
-	    return create_folder(service, team_drive, folder_name,
-				 parent_id)
+    # If we got 0 results back, then go create that folder
+    elif len(folders) == 0:
+	print("=== Folder not found -- need to create it")
+	return create_folder(service, team_drive, folder_name,
+			     parent_id)
 
-	# Otherwise, we found it.  Yay!
-	else:
-	    folder = folders[0]
-	    print('=== Found target folder: "{0}" (ID: {1})'
-		  .format(folder_name, folder['id']))
-	    return folder
-
-    except AccessTokenRefreshError:
-	# The AccessTokenRefreshError exception is raised if the
-	# credentials have been revoked by the user or they have
-	# expired.
-	print('The credentials have been revoked or expired, '
-	      'please re-run the application to re-authorize')
-	exit(1)
+    # Otherwise, we found it.  Yay!
+    else:
+	folder = folders[0]
+	print('=== Found target folder: "{0}" (ID: {1})'
+	      .format(folder_name, folder['id']))
+	return folder
 
 #-------------------------------------------------------------------
 
@@ -291,54 +340,73 @@ def watch_for_new_mp3s(service, team_drive, source_dir):
 	    del seen_files[file]
 
 	# Wait a little bit, and then check again
-	time.sleep(30)
+	time.sleep(dir_check_frequency)
 
 #-------------------------------------------------------------------
 
 def find_team_drive(service, target_name):
-    try:
-	page_token = None
-	while True:
-	    response = (service.teamdrives()
-			       .list(pageToken=page_token).execute())
-	    for team_drive in response.get('teamDrives', []):
-		if team_drive['name'] == target_name:
-		    print('=== Found target Team Drive: "{0}" (ID: {1})'
-			  .format(target_name, team_drive['id']))
-		    return team_drive
+    page_token = None
+    while True:
+	response = (service.teamdrives()
+		    .list(pageToken=page_token).execute())
+	for team_drive in response.get('teamDrives', []):
+	    if team_drive['name'] == target_name:
+		print('=== Found target Team Drive: "{0}" (ID: {1})'
+		      .format(target_name, team_drive['id']))
+		return team_drive
 
-	    page_token = response.get('nextPageToken', None)
-	    if page_token is None:
-		break
+	page_token = response.get('nextPageToken', None)
+	if page_token is None:
+	    break
 
-	# If we get here, we didn't find the target team drive
-	print("Error: Could not find the target Team Drive ({0})"
+    # If we get here, we didn't find the target team drive
+    diediedie("Error: Could not find the target Team Drive ({0})"
 	      .format(target_team_drive))
-	print("Error: Aborting")
-	exit(1)
-
-    except AccessTokenRefreshError:
-	# The AccessTokenRefreshError exception is raised if the
-	# credentials have been revoked by the user or they have
-	# expired.
-	print('The credentials have been revoked or expired, '
-	      'please re-run the application to re-authorize')
-	exit(1)
 
 #-------------------------------------------------------------------
 
 def main():
-    # Authorize the app and provide user consent to Google
-    app_cred = load_app_credentials()
-    user_cred = load_user_credentials(scope, app_cred)
-    service = authorize(user_cred)
+    # Put a loop around the whole program so that it can
+    # re-authenticate via the OAuth refresh token when possible.  Real
+    # errors will cause the script to abort, which will notify a human
+    # to fix whatever the problem.
 
-    # Find the target team drive to which we want to upload
-    team_drive = find_team_drive(service, target_team_drive)
+    last_auth = 0
+    auth_count = 0
+    while True:
+	try:
+	    # Authorize the app and provide user consent to Google
+	    app_cred = load_app_credentials()
+	    user_cred = load_user_credentials(scope, app_cred)
+	    service = authorize(user_cred)
 
-    # Endlessly watch for new files to appear in the source directory,
-    # and upload them to Google
-    watch_for_new_mp3s(service, team_drive, source_directory)
+            last_auth = time.clock()
+
+	    # Find the target team drive to which we want to upload
+	    team_drive = find_team_drive(service, target_team_drive)
+
+	    # Endlessly watch for new files to appear in the source
+	    # directory, and upload them to Google
+	    watch_for_new_mp3s(service, team_drive, source_directory)
+
+	except AccessTokenRefreshError:
+	    # The AccessTokenRefreshError exception is raised if the
+	    # credentials have been revoked by the user or they have
+	    # expired.
+	    # Try to re-auth.  If we fail to re-auth 3 times within 60
+	    # seconds, abort and let a human figure it out.
+            now = time.clock()
+            if (now - last_auth) > auth_grace_period:
+                last_auth = now
+                auth_count = 0
+                continue
+
+            else:
+                auth_count = auth_count + 1
+                if auth_count > auth_max_attempts:
+                    diediedie("Failed to authenticate to Google {0} times in {1} seconds.\nA human needs to figure this out."
+                              .format(auth_max_attempts, auth_grace_period))
+
 
 if __name__ == '__main__':
     main()
