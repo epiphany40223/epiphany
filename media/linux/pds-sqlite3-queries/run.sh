bad59c6725b26b8dc653208c18ece053b721bd0d
Jeff Squyres <jeff@squyres.com>
2017-12-22 15:52:02 -0500

    media/linux: rename PDS queries -> sync-mailman.py
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- media/linux/pds-sqlite3-queries/run.sh
+++ media/linux/pds-sqlite3-queries/run.sh
@@ -4,15 +4,15 @@ set -x
 
 base=/home/itadmin/git/epiphany/media/linux
 prog_dir=$base/pds-sqlite3-queries
-logfile=$base/logfile.txt
 sqlite_dir=$base/pds-data
 
 cd $prog_dir
 
 # Generate the list of email addresses from PDS data
-./pds-sqlite3-queries.py \
+mailman_logfile=$prog_dir/sync-mailman-logfile.txt
+./sync-mailman.py \
     --sqlite3-db=$sqlite_dir/pdschurch.sqlite3 \
-    --logfile=$logfile \
+    --logfile=$mailman_logfile \
     --verbose
 
 # This generated mailman-parishioner.txt.
