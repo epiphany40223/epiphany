3b6b4414dc6c78b17ce7c36771a35ca3ee57bc53
Jeff Squyres <jeff@squyres.com>
2018-12-19 07:40:59 -0500

    Google.py: add utility "retry calling API" function
    This was called "doit()" in several previous scripts: call a Google
    API multiple times, and in some kinds of failures, delay a little and
    retry up to N times.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- python/Google.py
+++ python/Google.py
@@ -2,6 +2,22 @@
 #
 # Utility Google functions
 #
+# Need to:
+#
+# pip3 install --upgrade httplib2
+# pip3 install --upgrade google-api-python-client oauth2client
+#
+
+import time
+import traceback
+
+import httplib2
+
+from pprint import pprint
+from pprint import pformat
+from apiclient.errors import HttpError
+
+# Globals
 
 mime_types = {
     'doc'        : 'application/vnd.google-apps.document',
@@ -26,3 +42,42 @@ scopes = {
     'group'   : 'https://www.googleapis.com/auth/apps.groups.settings',
     'reports' : 'https://www.googleapis.com/auth/admin.reports.audit.readonly',
 }
+
+####################################################################
+
+# Make a Google API call.  If it fails, try again.
+#
+def call_api(httpref, log, max_retries=3, can_fail=False):
+    log.debug("Executing Google API call (will try up to {count} times): {h}"
+              .format(count=max_retries, h=httpref))
+
+    for count in range(max_retries):
+        try:
+            ret = httpref.execute()
+            return ret
+
+        except HttpError as err:
+            log.debug("*** Got HttpError:")
+            log.debug(pformat(err))
+            if err.resp.status in [500, 503]:
+                log.debug("*** Seems recoverable; let's sleep and try again...")
+                time.sleep(5)
+                continue
+            elif err.resp.status == 403:
+                log.debug("*** Permission denied, but that's ok -- we'll skip it for now...")
+                return None
+            else:
+                log.debug("*** Doesn't seem recoverable (status {0}) -- aborting"
+                          .format(err.resp.status))
+                log.debug(err)
+                raise
+
+        except:
+            log.error("*** Some unknown error occurred")
+            log.error(sys.exc_info()[0])
+            raise
+
+    # If we get here, it's failed multiple times -- time to bail...
+    log.error("Error: we failed this API call {count} times; there's no reason to believe it'll work if we do it again..."
+              .format(count=max_retries))
+    exit(1)
