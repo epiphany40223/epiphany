e700a4c5e4ef1e9bca1f512ae7738a140332e04d
Jeff Squyres <jeff@squyres.com>
2018-12-22 12:41:55 -0500

    After making the nightly PDS archive, upload to Team Drive
    We keep running out of space on our small Linux VM.  So store the PDS
    data backups in a dedicated Google Team Drive, instead.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

---
+++ media/linux/google-drive-uploader/README.md
@@ -0,0 +1,38 @@
+# Google Uploader Python client
+
+## Instructions to setup Fedora
+
+```
+sudo yum install -y python3-google-api-client python3-httplib2
+```
+
+## Setting up credentials
+
+Setting up and downloading credentials from the Google cloud console:
+
+1. Go to console.cloud.google.com
+1. Make a project
+1. Go to "APIs and Credentials"
+1. Enable APIs and services
+   1. Enable the "Google Drive API"
+1. Credentials
+1. Make a new credentia / select "Help me Choose"
+   1. Select that we're using the Google Drive API
+   1. Select "Other UI (Windows, CLI tool)"
+   1. Select "User data"
+   1. Make the credentials
+   1. Download the JSON client secret file
+   1. The google uploader script defaults to looking for
+      `client_id.json` (vs. the lengthy, unique filename that you
+      likely downloaded).
+
+## Obtaininig specific user credentials
+
+Using the `client_id.json` that you just downloaded:
+
+1. Run the script on a machine where you have access to a web browser.
+   1. It will open a Google auth page on the browser.
+   1. Complete the google auth.
+   1. When done, a `user_credentials.json` file will be written.
+   1. This file can be copied to the machine where the script will be
+      run for real.
---
+++ media/linux/google-drive-uploader/google-drive-uploader.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3.6
+
+"""Script to upload specific files to Google Drive.
+
+This script developed and tested with Python 3.6.x.  It has not been
+tested with other versions (e.g., Python 2.7.x).
+
+"""
+
+import sys
+sys.path.insert(0, '../../../python')
+
+import os
+import mimetypes
+
+import ECC
+import Google
+import GoogleAuth
+
+from apiclient.http import MediaFileUpload
+from oauth2client import tools
+
+#===================================================================
+
+def gd_find_folder(service, folder_id, log):
+    # See if this folder ID exists (and is a folder)
+    try:
+        response = (service.files()
+                    .get(fileId=folder_id,
+                         supportsTeamDrives=True).execute())
+    except:
+        log.error("Failed to find Google Drive ID {f}".format(f=folder_id))
+        exit(1)
+
+    log.debug("Validated Google Drive destination ID exists: {id}"
+              .format(id=folder_id))
+
+    mime = response.get('mimeType', [])
+    if not mime:
+        log.error("Failed to verify that Google Drive ID is a folder")
+        exit(1)
+
+    if mime != Google.mime_types['folder']:
+        log.error("Destination Google Drive ID is not a folder")
+        log.error("It's actually: {m}".format(m=mime))
+        exit(1)
+
+    log.debug("Validated Google Drive destination ID is a folder")
+
+    return response
+
+#===================================================================
+
+def gd_upload_file(service, dest_folder, upload_filename, log):
+    basename = os.path.basename(upload_filename)
+    mime     = mimetypes.guess_type('file://{f}'
+                                    .format(f=upload_filename))
+    if mime == (None, None):
+        if '.sqlite3' in upload_filename:
+            mime = 'application/x-sqlite3'
+        else:
+            mime = 'text/plain'
+        log.debug('Got no mime type: assume {m}'.format(m=mime))
+
+    log.debug('Uploading file "{base}" (Google Drive folder ID: {folder}, MIME type {m})'
+              .format(base=basename,
+                      folder=dest_folder['id'],
+                      m=mime))
+    metadata = {
+        'name'     : basename,
+        'mimeType' : mime,
+        'parents'  : [ dest_folder['id'] ]
+    }
+    media = MediaFileUpload(upload_filename,
+                            mimetype=mime,
+                            resumable=True)
+    file = service.files().create(body=metadata,
+                                  media_body=media,
+                                  supportsTeamDrives=True,
+                                  fields='name,id,webContentLink,webViewLink').execute()
+    log.debug('Successfully uploaded file: "{f}" --> Google Drive file ID {id}'
+              .format(f=basename, id=file['id']))
+
+####################################################################
+#
+# Setup functions
+#
+####################################################################
+
+def add_cli_args():
+    tools.argparser.add_argument('--app-id',
+                                 default='client_id.json',
+                                 help='Filename containing Google application credentials')
+    tools.argparser.add_argument('--user-credentials',
+                                 default='user-credentials.json',
+                                 help='Filename containing Google user credentials')
+
+    tools.argparser.add_argument('files',
+                                 metavar='file',
+                                 nargs='+',
+                                 help='File (or files) to upload to Google Drive')
+
+    tools.argparser.add_argument('--dest',
+                                 required=True,
+                                 help='ID of target Google Folder')
+
+    tools.argparser.add_argument('--verbose',
+                                 required=False,
+                                 action='store_true',
+                                 default=True,
+                                 help='If enabled, emit extra status messages during run')
+    tools.argparser.add_argument('--debug',
+                                 required=False,
+                                 action='store_true',
+                                 default=False,
+                                 help='If enabled, emit even more extra status messages during run')
+
+    args = tools.argparser.parse_args()
+
+    # --debug also implies --verbose
+    if args.debug:
+        args.verbose = True
+
+    # Sanity check that the specified files all exist
+    for f in args.files:
+        if not os.path.exists(f):
+            print("File does not exist: {f}".format(f=f))
+            exit(1)
+
+    return args
+
+####################################################################
+#
+# Main
+#
+####################################################################
+
+def main():
+    args = add_cli_args()
+    log  = ECC.setup_logging(info=args.verbose,
+                             debug=args.verbose,
+                             logfile=None)
+
+    apis = {
+        'drive' : { 'scope'       : Google.scopes['drive'],
+                    'api_name'    : 'drive',
+                    'api_version' : 'v3',
+        }
+    }
+    services = GoogleAuth.service_oauth_login(apis,
+                                              app_json=args.app_id,
+                                              user_json=args.user_credentials,
+                                              log=log)
+    service  = services['drive']
+
+    dest_folder = gd_find_folder(service, args.dest, log)
+
+    for f in args.files:
+        log.info("Uploading file: {f}".format(f=f))
+        gd_upload_file(service, dest_folder, f, log)
+
+    log.info("Finished uploading files")
+
+if __name__ == '__main__':
+    main()
