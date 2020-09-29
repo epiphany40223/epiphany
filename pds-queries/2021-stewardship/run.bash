c6b8674138ec43af9e1d481e0eb63a4d8cffa6ed
Jeff Squyres <jeff@squyres.com>
2020-09-29 06:36:53 -0400

    2021 eStewardship: minor updates
    Minor updates for the 1st reminder email for eStewardship 2021.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- pds-queries/2021-stewardship/run.bash
+++ pds-queries/2021-stewardship/run.bash
@@ -5,7 +5,7 @@ set -euo pipefail
 # This is a convenience script to invoke the "send all the emails"
 # script.
 #
-# You should run copy-while-running.zsh at the same time as
+# You should run copy-while-running.sh at the same time as
 # this script!!
 
 start=`date`
@@ -13,19 +13,21 @@ echo "========================================================="
 echo "Starting at: $start"
 echo "========================================================="
 
-email_file=email-initial.html
-#email_file=email-1st-reminder.html
+#email_file=email-initial.html
+email_file=email-1st-reminder.html
 
 #email_addr=jeff@squyres.com
-email_addr=jsquyres@epiphanycatholicchurch.org
-filter="--email $email_addr"
+#email_addr=jsquyres@epiphanycatholicchurch.org
+#filter="--email $email_addr"
 
-# JMS Send them all!
 #filter="--all"
 
 #email_file=email-test.html
 #env_ids_file=id.txt
 #filter="--env-id-file $env_ids_file"
+
+#filter="--unsubmitted"
+filter="--unsubmitted --do-not-send"
 
 ./make-and-send-emails.py \
         --smtp-auth smtp-auth.txt \
