373f561c83d22e0db7c07e4656c05d51554734f1
franciscogallardo <fragalcer@gmail.com>
2020-10-15 22:20:55 +1100

    Improved code quality.
    Signed-off-by: franciscogallardo <fragalcer@gmail.com>

--- python/Google.py
+++ python/Google.py
@@ -20,29 +20,30 @@ from apiclient.errors import HttpError
 # Globals
 
 mime_types = {
-    'doc'        : 'application/vnd.google-apps.document',
-    'sheet'      : 'application/vnd.google-apps.spreadsheet',
-    'folder'     : 'application/vnd.google-apps.folder',
+    'doc': 'application/vnd.google-apps.document',
+    'sheet': 'application/vnd.google-apps.spreadsheet',
+    'folder': 'application/vnd.google-apps.folder',
 
-    'json'       : 'application/json',
+    'json': 'application/json',
 
-    'mp3'        : 'audio/mpeg',
+    'mp3': 'audio/mpeg',
 
-    'csv'        : 'text/csv',
+    'csv': 'text/csv',
 
     # JMS this is probably a lie, but it's useful for comparisons
-    'team_drive' : 'application/vnd.google-apps.team_drive',
+    'team_drive': 'application/vnd.google-apps.team_drive',
 }
 
 # Scopes documented here:
 # https://developers.google.com/drive/v3/web/about-auth
 scopes = {
-    'drive'    : 'https://www.googleapis.com/auth/drive',
-    'admin'    : 'https://www.googleapis.com/auth/admin.directory.group',
-    'group'    : 'https://www.googleapis.com/auth/apps.groups.settings',
-    'reports'  : 'https://www.googleapis.com/auth/admin.reports.audit.readonly',
-    'calendar' : 'https://www.googleapis.com/auth/calendar',
+    'drive': 'https://www.googleapis.com/auth/drive',
+    'admin': 'https://www.googleapis.com/auth/admin.directory.group',
+    'group': 'https://www.googleapis.com/auth/apps.groups.settings',
+    'reports': 'https://www.googleapis.com/auth/admin.reports.audit.readonly',
+    'calendar': 'https://www.googleapis.com/auth/calendar',
 }
+
 
 ####################################################################
 
@@ -79,6 +80,7 @@ def call_api(httpref, log, max_retries=3, can_fail=False):
             raise
 
     # If we get here, it's failed multiple times -- time to bail...
-    log.error("Error: we failed this API call {count} times; there's no reason to believe it'll work if we do it again..."
-              .format(count=max_retries))
+    log.error(
+        "Error: we failed this API call {count} times; there's no reason to believe it'll work if we do it again..."
+        .format(count=max_retries))
     exit(1)
