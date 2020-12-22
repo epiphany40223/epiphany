842118b267c140f2275fa01c7e3fcf3115852fa4
Jeff Squyres <jeff@squyres.com>
2020-12-22 18:48:28 -0500

    Move Google credential files
    Move the google app and user credential files to $HOME/credentials.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- media/linux/pds-sqlite3-queries/run.sh
+++ media/linux/pds-sqlite3-queries/run.sh
@@ -22,10 +22,13 @@ cd $prog_dir
 
 # Generate the list of email addresses from PDS data and sync
 google_logfile=$HOME/logfiles/linux/sync-google-group/sync-google-group-logfile.txt
+cred_dir=$HOME/credentials/pds-sqlite3-queries
 ./sync-google-group.py \
     --smtp-auth-file $HOME/credentials/smtp-auth.txt \
     --slack-token-file $HOME/credentials/slack-token.txt \
     --sqlite3-db=$sqlite_dir/pdschurch.sqlite3 \
+    --app-id $cred_dir/client_id.json \
+    --user-credentials $cred_dir/user-credentials.json \
     --logfile=$google_logfile \
     --verbose
 
@@ -43,23 +46,24 @@ h=`date '+%H'`
 m=`date '+%M'`
 if test $h -eq 2 -a $m -lt 15; then
     roster_logfile=$HOME/logfiles/linux/ministry-roster/ministry-roster-logfile.txt
+    cred_dir=$HOME/credentials/google-drive-uploader
     ./create-ministry-rosters.py \
 	--sqlite3-db=$sqlite_dir/pdschurch.sqlite3 \
 	--logfile=$roster_logfile \
-	--app-id ../google-drive-uploader/google-uploader-client-id.json \
-	--user-credentials ../google-drive-uploader/google-uploader-user-credentials.json
+	--app-id $cred_dir/google-uploader-client-id.json \
+	--user-credentials $cred_dir/google-uploader-user-credentials.json
 
     roster_logfile=$HOME/logfiles/linux/training-roster/trailing-roster-logfile.txt
     ./create-training-rosters.py \
 	--sqlite3-db=$sqlite_dir/pdschurch.sqlite3 \
 	--logfile=$roster_logfile \
-	--app-id ../google-drive-uploader/google-uploader-client-id.json \
-	--user-credentials ../google-drive-uploader/google-uploader-user-credentials.json
+	--app-id $cred_dir/google-uploader-client-id.json \
+	--user-credentials $cred_dir/google-uploader-user-credentials.json
 
     ppc_logfile=$HOME/logfiles/linux/ppc-feedback/ppc-feedback-logfile.txt
     ./ppc-feedback-google-group.py \
         --smtp-auth-file $HOME/credentials/smtp-auth.txt \
         --logfile=$ppc_logfile \
-        --app-id $HOME/credentials/client_id-ppc-feedback.json \
-        --user-credentials $HOME/credentials/user-credentials-ppc-feedback.json
+	--app-id $cred_dir/google-uploader-client-id.json \
+	--user-credentials $cred_dir/google-uploader-user-credentials.json
 fi
