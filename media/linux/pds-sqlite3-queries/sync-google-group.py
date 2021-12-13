f31f3db30c7b5fd99d1818ca9cfb7201787128d6
Jeff Squyres <jeff@squyres.com>
2021-12-13 13:22:32 -0500

    sync-goog: Add 301 Audio/Visual/Lighting ministry
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- media/linux/pds-sqlite3-queries/sync-google-group.py
+++ media/linux/pds-sqlite3-queries/sync-google-group.py
@@ -164,6 +164,11 @@ def get_synchronizations():
         #############################
 
         {
+            'ministries' : [ '301-Audio/Visual/Light Minstry' ],
+            'ggroup'     : f'audio-visual-lighting-ministry{ecc}',
+            'notify'     : f'director-worship{ecc},pds-google-sync{ecc}',
+        },
+        {
             'ministries' : [ '302-Bread Baking Ministry' ],
             'ggroup'     : f'breadmakers{ecc}',
             'notify'     : f'director-worship{ecc},pds-google-sync{ecc}',
