44897b3d73dfe1b6703479fa4c58d1e79c29e3fe
Jeff Squyres <jeff@squyres.com>
2022-12-18 19:22:32 -0500

    sync-google-group: Add wedding-assistant-chair
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- media/linux/pds-sqlite3-queries/sync-google-group.py
+++ media/linux/pds-sqlite3-queries/sync-google-group.py
@@ -435,7 +435,7 @@ def get_synchronizations():
         {
             'keywords'   : [ 'Wedding Ministries email list' ],
             'ggroup'     : f'wedding-ministries{ecc}',
-            'notify'     : f'director-worship{ecc},pds-google-sync{ecc}',
+            'notify'     : f'director-worship{ecc},wedding-assistant-chair{ecc},pds-google-sync{ecc}',
         },
         {
             'keywords'   : [ 'Weekday Mass Email' ],
@@ -495,6 +495,13 @@ def get_synchronizations():
                                'kwargs' : { "ministry_prefix" : "300"},
                                'purpose' : "Worship/art&environment ministry chair" }, ],
             'ggroup'     : f'worship-art-and-environment-chair{ecc}',
+            'notify'     : f'director-worship{ecc},pds-google-sync{ecc}',
+        },
+        {
+            'functions'  : [ { 'func' : find_ministry_chair,
+                               'kwargs' : { "ministry_prefix" : "307"},
+                               'purpose' : "Wedding assistant ministry chair" }, ],
+            'ggroup'     : f'wedding-assistant-chair{ecc}',
             'notify'     : f'director-worship{ecc},pds-google-sync{ecc}',
         },
         {
